900066e2c4c9c6fd1f3be218a017a30d08223db3
Laurie White <laurie.white@gmail.com>
2015-08-05 20:17:44 -0400

    Project 1 Popular Movies
    Original Commit of the Popular Movies Project for the Android Nanodegree
    at Udacity.

---
+++ app/src/main/java/com/example/android/project1movies/app/MainActivity.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2015 Laurie White (copyright@lauriewhite.org)
+ *
+ * Project based on Project Sunshine from Udacity's "Developing
+ * Android Apps" course at
+ * https://www.udacity.com/course/developing-android-apps--ud853
+ *
+ */
+package com.example.android.project1movies.app;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * A solution to project 1 of the Android Nanodegree by Udacity, as
+ * described in
+ * https://docs.google.com/document/d/1ZlN1fUsCSKuInLECcJkslIqvpKlP7jWL2TP9m6UiA6I/pub?embedded=true
+ * @author Laurie White
+ * @version 5 August 2015
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/com/example/android/project1movies/app/MovieData.java
@@ -0,0 +1,150 @@
+package com.example.android.project1movies.app;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/**
+ * A class to represent the data about one movie.
+ * This class is responsible for parsing the JSON data
+ * about a single movie.  As the project is expanded,
+ * this class will also expand to hold more information.
+ * @author Laurie White
+ * @version 7/27/2015.
+ */
+public class MovieData implements Parcelable {
+
+    //  TODO: Extract the data for: title, release date, movie poster, vote average, and plot synopsis.
+
+    //  TODO: Consider reformatting release date
+    //  TODO: vote_average
+    //  TODO: plot synopsis
+    //  TODO: vote_count
+
+    private final String LOG_TAG = MovieData.class.getSimpleName();
+
+    public MovieData (JSONObject jsonData) {
+        final String TMD_title = "original_title";
+        final String TMD_image = "poster_path";
+        final String TMD_vote_average = "vote_average";
+        final String TMD_vote_count = "vote_count";
+        final String TMD_release_date = "release_date";
+        final String TMD_synopsis = "overview";
+
+
+        try{
+            mTitle = jsonData.getString(TMD_title);
+        } catch (JSONException e) {
+            Log.w(LOG_TAG, "No name for movie");
+        }
+        try{
+            mPoster = jsonData.getString(TMD_image);
+        } catch (JSONException e) {
+            Log.w(LOG_TAG, "No poster for movie");
+        }
+
+        try{
+            mReleaseDate = jsonData.getString(TMD_release_date);
+        } catch (JSONException e) {
+            Log.w(LOG_TAG, "No release date for movie");
+        }
+
+        try {
+            mVoteAverage = jsonData.getDouble(TMD_vote_average);
+        } catch (JSONException e) {
+            Log.w (LOG_TAG, "No vote average for movie");
+        }
+
+        try {
+            mOverview = jsonData.getString(TMD_synopsis);
+        } catch (JSONException e) {
+            Log.w(LOG_TAG, "No overview for movie");
+        }
+
+        try {
+            mVotes = jsonData.getInt(TMD_vote_count);
+        } catch (JSONException e) {
+            Log.w(LOG_TAG, "No number of votes for movie");
+        }
+    }
+
+    private String mOverview;
+    public String getOverview() {
+        return mOverview;
+    }
+
+    private double mVoteAverage;
+    public double getVoteAverage() {
+        return mVoteAverage;
+    }
+    private String mPoster;
+    //  TODO: Well, this is interesting...since I can't access the element,
+    //  I can't build the whole API, dammit
+
+    /**
+     * Give the relative path to the poster for this movie
+     * @return the relative path to the poster
+     */
+    public String getPosterURL() {
+        return mPoster;
+    }
+
+    private String mTitle;
+    public String getTitle() {
+        return mTitle;
+    }
+
+    private String mReleaseDate;
+    public String getReleaseDate() {
+        return mReleaseDate;
+    }
+
+    private int mVotes;
+    public int getVotes() {
+        return mVotes;
+    }
+    /*  So, to pass this entire class to an Activity to display the details,
+        it looks like I should make it a Parcelable (based on
+        http://stackoverflow.com/questions/2139134/how-to-send-an-object-from-one-android-activity-to-another-using-intents
+        ).  So, here's all the methods needed to implement a Parcelable.
+        (And I thought Comparable was a pain to pronounce.)
+     */
+
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(mPoster);
+        out.writeString(mTitle);
+        out.writeString(mReleaseDate);
+        out.writeString(mOverview);
+        out.writeInt(mVotes);
+        out.writeDouble(mVoteAverage);
+    }
+
+    public static final Parcelable.Creator<MovieData> CREATOR
+            = new Parcelable.Creator<MovieData>() {
+        public MovieData createFromParcel(Parcel in) {
+            return new MovieData(in);
+        }
+
+        public MovieData[] newArray(int size) {
+            return new MovieData[size];
+        }
+    };
+
+    private MovieData(Parcel in) {
+        mPoster = in.readString();
+        mTitle = in.readString();
+        mReleaseDate = in.readString();
+        mOverview = in.readString();
+        mVotes = in.readInt();
+        mVoteAverage = in.readDouble();
+    }
+
+}
